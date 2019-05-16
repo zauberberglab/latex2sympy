dd0ed86a54fbc8ae8114cb6789c06c37bf18dd19
Evan Tragesser <etragess@purdue.edu>
2019-05-16 16:25:54 -0400

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name="latex2sympy",
+	packages=['gen'],
+	py_modules=['asciimath_printer', 'process_latex'],
+	install_requires=[
+		'sympy==1.4',
+		'antlr4-python3-runtime>=4.7.2'
+	]
+)

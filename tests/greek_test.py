eb05d5fcd5134f747b0a766038a40d499042be38
Lily Carlson <carls118@purdue.edu>
2019-06-18 12:28:24 -0400

    initial attempt at greek test - char codes are failing

---
+++ tests/greek_test.py
@@ -0,0 +1,21 @@
+from .context import assert_equal
+import pytest
+from sympy import Symbol, abc
+
+abc.alpha
+abc.theta
+abc.epsilon
+
+# '\\char\"000190' | //Epsilon
+epsilon_upper = Symbol('Epsilon', real=True)
+# '\\epsilon' |
+epsilon_lower = Symbol('epsilon', real=True)
+# '\\varepsilon' |
+varepsilon = Symbol('varepsilon', real=True)
+
+def test_greek_epsilon():
+	assert_equal("\\epsilon", epsilon_lower)
+def test_greek_epsilon_upper():
+	assert_equal("\\char\"000190", epsilon_upper)
+def test_greek_varepsilon():
+	assert_equal('\\varepsilon', varepsilon)

50c5ea283d11113fcebba7984070ea2b02dcfcb1
Will Grauvogel <builtbywill@gmail.com>
2019-06-21 10:55:33 -0400

    update test with greek letter test

--- tests/variable_test.py
+++ tests/variable_test.py
@@ -9,7 +9,7 @@ def test_variable():
 	assert_equal("\\variable{x}", Symbol('x' + hashlib.md5('x'.encode()).hexdigest(), real=True))
 def test_variable_expr():
 	assert_equal("4\\cdot\\variable{x}", 4*Symbol('x' + hashlib.md5('x'.encode()).hexdigest(), real=True))
-def test_variable_with_command():
-	assert_equal("4\\cdot\\variable{alpha}\\alpha", 4*Symbol('alpha' + hashlib.md5('alpha'.encode()).hexdigest(), real=True)*Symbol('alpha', real=True))
+def test_variable_with_greek_letter_command():
+	assert_equal("4\\cdot\\variable{\\alpha}\\alpha", 4*Symbol('\\alpha' + hashlib.md5('\\alpha'.encode()).hexdigest(), real=True)*Symbol('alpha', real=True))
 def test_variable_complex():
 	assert_equal("4\\cdot\\variable{value1}\\frac{\\variable{value_2}}{\\variable{a}}\\cdot x^2", 4*Symbol('value1' + hashlib.md5('value1'.encode()).hexdigest(), real=True)*Symbol('value_2' + hashlib.md5('value_2'.encode()).hexdigest(), real=True)/Symbol('a' + hashlib.md5('a'.encode()).hexdigest(), real=True)*x**2)

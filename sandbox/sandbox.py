9a341230774045c02ed99849f4e896e807d27221
Will Grauvogel <builtbywill@gmail.com>
2019-11-07 12:03:11 -0500

    update sandbox

--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -1,37 +1,61 @@
-from sympy import Symbol, sympify, simplify, Eq, factor, srepr, pi
+from sympy import Symbol, sympify, simplify, Eq, factor, srepr, pi, Number, Mul, Pow, Integer
 from latex2sympy import process_sympy
 
-# x = Symbol('x', real=True)
+#
+# Number Testing
+#
 
-numeric_responses = ['1', '1.0', '-1', '-1.0', '.5', '-.5', '3x10^3', '3E3', '3,000x10^{-3}', '0.5E-1', '\\frac{1}{3}', '(5\\times 3)^3', '\\sin(1)']
+# print(process_sympy('(-7.13)(1.5)'))
+# print(srepr(process_sympy('(-7.13)(1.5)')))
 
-for latex in numeric_responses:
-    parsed = process_sympy(latex)
-    print('latex: ', latex)
-    print('sympy: ', parsed)
-    print('is_number: ', parsed.is_number)
-    print('is_Number: ', parsed.is_Number)
-    print('srepr: ', srepr(parsed))
-    print('-----------------------------------------------------')
+# numeric_responses = ['1', '1.0', '-1', '-1.0', '.5', '-.5', '3x10^3', '3E3', '3,000x10^{-3}', '0.5E-1', '\\frac{1}{3}', '(5\\times 3)^3', '\\sin(1)']
+# for latex in numeric_responses:
+#     parsed = process_sympy(latex)
+#     print('latex: ', latex)
+#     print('sympy: ', parsed)
+#     print('is_number: ', parsed.is_number)
+#     print('is_Number: ', parsed.is_Number)
+#     print('srepr: ', srepr(parsed))
+#     print('-----------------------------------------------------')
 
-# answer_sets = [
-#     {'correct_answer': '(x-y)(x+2y)', 'student_answers': ['x^2+xy-2y^2', '(x-y)(x+2y)', '(x+2y)(x-y)', '(2y+x)(-y+x)']}
-# ]
+#
+# Equality Testing
+#
 
-# for answer_set in answer_sets:
-#     correct_answer = answer_set['correct_answer']
-#     correct_answer_parsed = process_sympy(answer_set['correct_answer'])
-#     for student_answer in answer_set['student_answers']:
-#         student_answer_parsed = process_sympy(student_answer)
-#         print('correct_answer (c): ', correct_answer)
-#         print('student_answer (a): ', student_answer)
-#         print('')
-#         print('Expression Tree (srepr(c) == srepr(a)) =>', srepr(correct_answer_parsed) == srepr(student_answer_parsed))
-#         print('Structural (c == a) =>', correct_answer_parsed == student_answer_parsed)
-#         print('Numeric Substitution (c.equals(s)) =>', correct_answer_parsed.equals(student_answer_parsed))
-#         print('Symbolic (simplify(c - s) == 0) =>', simplify(correct_answer_parsed - student_answer_parsed) == 0)
-#         print('-----------------------------------------------------')
+answer_sets = [
+    {
+        'correct_answer': '(x-y)(x+2y)',
+        'student_answers': [
+            'x^2+xy-2y^2',
+            '(x-y)(x+2y)',
+            '(x+2y)(x-y)',
+            '(2\\times y+x)(-y+x)',
+            '(y\\cdot 2+x)(-y+x)'
+        ]
+    },
+    {
+        'correct_answer': '2\\pi \\variable{r}^2',
+        'student_answers': [
+            '2\\pi \\variable{r}^2',
+            '\\pi 2\\variable{r}^2',
+            '2\\times \\pi \\times \\variable{r}^2'
+        ]
+    }
+]
 
-# answer = parsed.evalf()
-# answer = parsed.evalf(subs={x: '1'})
-# print('answer:', answer)
+for answer_set in answer_sets:
+    correct_answer = answer_set['correct_answer']
+    correct_answer_parsed = process_sympy(answer_set['correct_answer'])
+    for student_answer in answer_set['student_answers']:
+        student_answer_parsed = process_sympy(student_answer)
+        print('correct_answer (c): ', correct_answer)
+        print('student_answer (a): ', student_answer)
+        print('')
+        print('Expression Tree (srepr(c) == srepr(a)) =>', srepr(correct_answer_parsed) == srepr(student_answer_parsed))
+        print('srepr(c) =>', srepr(correct_answer_parsed))
+        print('srepr(a) =>', srepr(student_answer_parsed))
+        print('')
+        # print('Structural (c == a) =>', correct_answer_parsed == student_answer_parsed)
+        # print('Numeric Substitution (c.equals(s)) =>', correct_answer_parsed.equals(student_answer_parsed))
+        print('Symbolic (simplify(c - s) == 0) =>', simplify(correct_answer_parsed - student_answer_parsed) == 0)
+        print('-----------------------------------------------------')

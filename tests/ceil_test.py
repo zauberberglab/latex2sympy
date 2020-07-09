2937e3d00a9957c879367e5d7594ad0fd4e2cace
Prakrit <pduangsu@purdue.edu>
2020-07-09 01:36:44 -0400

    added ceil symbol to ceil_test

--- tests/ceil_test.py
+++ tests/ceil_test.py
@@ -29,6 +29,17 @@ def test_ceil_usual():
     assert_equal("\\operatorname{ceil}(\\pi)", ceiling(pi))
     assert_equal("\\operatorname{ceil}(\\sqrt{100})", ceiling(sqrt(100)))
 
+    assert_equal("\\lceil 1.1\\rceil", ceiling(1.1))
+    assert_equal("\\lceil 6.9\\rceil", ceiling(6.9))
+    assert_equal("\\lceil 3.5\\rceil", ceiling(3.5))
+    assert_equal("\\lceil 8\\rceil", ceiling(8))
+    assert_equal("\\lceil 0\\rceil", ceiling(0))
+    assert_equal("\\lceil 290348E32\\rceil", ceiling(Rational('290348E32')))
+    assert_equal("\\lceil 1237.293894239480234\\rceil", ceiling(Rational('1237.293894239480234')))
+    assert_equal("\\lceil 8623.4592104E-2\\rceil", ceiling(Rational('8623.4592104E-2')))
+    assert_equal("\\lceil \\pi\\rceil", ceiling(pi))
+    assert_equal("\\lceil \\sqrt{100}\\rceil", ceiling(sqrt(100)))
+
 
 def test_ceil_negative():
     assert_equal("\\ceil(-9.4)", ceiling(-9.4))
@@ -42,6 +53,12 @@ def test_ceil_negative():
     assert_equal("\\operatorname{ceil}(-\\sqrt{5})", ceiling(-sqrt(5)))
     assert_equal("\\operatorname{ceil}(-324E-3)", ceiling(Rational('-324E-3')))
     assert_equal("\\operatorname{ceil}(-0.23)", ceiling(-0.23))
+
+    assert_equal("\\lceil -9.4\\rceil", ceiling(-9.4))
+    assert_equal("\\lceil -35.9825\\rceil", ceiling(-35.9825))
+    assert_equal("\\lceil -\\sqrt{5}\\rceil", ceiling(-sqrt(5)))
+    assert_equal("\\lceil -324E-3\\rceil", ceiling(Rational('-324E-3')))
+    assert_equal("\\lceil -0.23\\rceil", ceiling(-0.23))
 
 
 def test_ceil_fraction():
@@ -59,6 +76,13 @@ def test_ceil_fraction():
     assert_equal("\\operatorname{ceil}(-325/3)", ceiling(Rational('-325/3')))
     assert_equal("\\operatorname{ceil}(\\pi/2)", ceiling(pi / 2))
 
+    assert_equal("\\lceil 1/2\\rceil", ceiling(Rational('1/2')))
+    assert_equal("\\lceil 6/2\\rceil", ceiling(Rational('6/2')))
+    assert_equal("\\lceil 9/5\\rceil", ceiling(Rational('9/5')))
+    assert_equal("\\lceil -42/6\\rceil", ceiling(Rational('-42/6')))
+    assert_equal("\\lceil -325/3\\rceil", ceiling(Rational('-325/3')))
+    assert_equal("\\lceil \\pi/2\\rceil", ceiling(pi / 2))
+
 
 def test_ceil_expr():
     assert_equal("\\ceil((1+6)/3)", ceiling(Rational(1 + 6, 3)))
@@ -73,6 +97,12 @@ def test_ceil_expr():
     assert_equal("38+\\operatorname{ceil}(15-2.3)", 38 + ceiling(15 - Rational('2.3')))
     assert_equal("\\sqrt{\\operatorname{ceil}(99.9999999999999)}", sqrt(ceiling(Rational('99.9999999999999'))))
 
+    assert_equal("\\lceil (1+6)/3\\rceil", ceiling(Rational(1 + 6, 3)))
+    assert_equal("\\lceil 1+6/3\\rceil", ceiling(1 + Rational('6/3')))
+    assert_equal("\\lceil 7*4/5\\rceil * 2", ceiling(7 * 4 / 5) * 2)
+    assert_equal("38+\\lceil 15-2.3\\rceil", 38 + ceiling(15 - Rational('2.3')))
+    assert_equal("\\sqrt{\\lceil 99.9999999999999\\rceil}", sqrt(ceiling(Rational('99.9999999999999'))))
+
 
 def test_ceil_symbol():
     assert_equal("\\ceil(x)", ceiling(x), symbolically=True)
@@ -86,3 +116,9 @@ def test_ceil_symbol():
     assert_equal("\\operatorname{ceil}(9x/4)", ceiling(9 * x / 4), symbolically=True)
     assert_equal("\\operatorname{ceil}(y\\pi)", ceiling(y * pi), symbolically=True)
     assert_equal("\\operatorname{ceil}(2y-y-y)", ceiling(2 * y - y - y), symbolically=True)
+
+    assert_equal("\\lceil x\\rceil", ceiling(x), symbolically=True)
+    assert_equal("\\lceil x + y\\rceil", ceiling(x + y), symbolically=True)
+    assert_equal("\\lceil 9x/4\\rceil", ceiling(9 * x / 4), symbolically=True)
+    assert_equal("\\lceil y\\pi\\rceil", ceiling(y * pi), symbolically=True)
+    assert_equal("\\lceil 2y-y-y\\rceil", ceiling(2 * y - y - y), symbolically=True)

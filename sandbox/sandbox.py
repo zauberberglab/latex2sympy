04e139b9f2ce719011646828e8e7419db5af6c0f
Will Grauvogel <builtbywill@gmail.com>
2020-07-09 15:25:36 -0400

    copy sandbox files from old branch

--- sandbox/sandbox.py
+++ sandbox/sandbox.py
@@ -1,12 +1,16 @@
-from sympy import Symbol, sympify, simplify, Eq, factor, srepr, pi, Number, Mul, Pow, Integer
+from sympy import *
 from latex2sympy import process_sympy
 
-#
-# Number Testing
-#
 
-# print(process_sympy('(-7.13)(1.5)'))
-# print(srepr(process_sympy('(-7.13)(1.5)')))
+# latex = '\\variable{a}^{\\variable{b}}'
+# variables = {'a': process_sympy('658.95998'), 'b': process_sympy('185083.8060')}
+# c_ans_expr = process_sympy(latex, variables)
+# print(c_ans_expr)
+# print(srepr(c_ans_expr))
+# c_ans = c_ans_expr.doit(deep=False).evalf(chop=True)
+# print(c_ans)
+# print(srepr(c_ans))
+
 
 # numeric_responses = ['1', '1.0', '-1', '-1.0', '.5', '-.5', '3x10^3', '3E3', '3,000x10^{-3}', '0.5E-1', '\\frac{1}{3}', '(5\\times 3)^3', '\\sin(1)']
 # for latex in numeric_responses:
@@ -17,51 +21,3 @@ from latex2sympy import process_sympy
 #     print('is_Number: ', parsed.is_Number)
 #     print('srepr: ', srepr(parsed))
 #     print('-----------------------------------------------------')
-
-#
-# Equality Testing
-#
-
-answer_sets = [
-    {
-        'correct_answer': '(x-y)(x+2y)',
-        'student_answers': [
-            'x^2+xy-2y^2',
-            '(x-y)(x+2y)',
-            '(x+2y)(x-y)',
-            '(2\\times y+x)(-y+x)',
-            '(y\\cdot 2+x)(-y+x)'
-        ]
-    },
-    {
-        'correct_answer': '2\\pi \\variable{r}^2',
-        'student_answers': [
-            '2\\pi \\variable{r}^2',
-            '\\pi 2\\variable{r}^2',
-            '2\\times \\pi \\times \\variable{r}^2'
-        ]
-    },
-    {
-        'correct_answer': '2x - 3y',
-        'student_answers': [
-            '-3y + 2x'
-        ]
-    }
-]
-
-for answer_set in answer_sets:
-    correct_answer = answer_set['correct_answer']
-    correct_answer_parsed = process_sympy(answer_set['correct_answer'])
-    for student_answer in answer_set['student_answers']:
-        student_answer_parsed = process_sympy(student_answer)
-        print('correct_answer (c): ', correct_answer)
-        print('student_answer (a): ', student_answer)
-        print('')
-        print('Expression Tree (srepr(c) == srepr(a)) =>', srepr(correct_answer_parsed) == srepr(student_answer_parsed))
-        print('srepr(c) =>', srepr(correct_answer_parsed))
-        print('srepr(a) =>', srepr(student_answer_parsed))
-        print('')
-        # print('Structural (c == a) =>', correct_answer_parsed == student_answer_parsed)
-        # print('Numeric Substitution (c.equals(s)) =>', correct_answer_parsed.equals(student_answer_parsed))
-        print('Symbolic (simplify(c - s) == 0) =>', simplify(correct_answer_parsed - student_answer_parsed) == 0)
-        print('-----------------------------------------------------')

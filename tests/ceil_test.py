354d7c3d0f1de1f134fdec838a4ccc87b4d8a0e7
Prakrit <pduangsu@purdue.edu>
2020-07-14 11:54:09 -0400

    added Unicode floor and ceil symbols

--- tests/ceil_test.py
+++ tests/ceil_test.py
@@ -40,6 +40,17 @@ def test_ceil_usual():
     assert_equal("\\lceil \\pi\\rceil", ceiling(pi))
     assert_equal("\\lceil \\sqrt{100}\\rceil", ceiling(sqrt(100)))
 
+    assert_equal("⌈1.1⌉", ceiling(1.1))
+    assert_equal("⌈6.9⌉", ceiling(6.9))
+    assert_equal("⌈3.5⌉", ceiling(3.5))
+    assert_equal("⌈8⌉", ceiling(8))
+    assert_equal("⌈0⌉", ceiling(0))
+    assert_equal("⌈290348E32⌉", ceiling(Rational('290348E32')))
+    assert_equal("⌈1237.293894239480234⌉", ceiling(Rational('1237.293894239480234')))
+    assert_equal("⌈8623.4592104E-2⌉", ceiling(Rational('8623.4592104E-2')))
+    assert_equal("⌈\\pi⌉", ceiling(pi))
+    assert_equal("⌈\\sqrt{100}⌉", ceiling(sqrt(100)))
+
 
 def test_ceil_negative():
     assert_equal("\\ceil(-9.4)", ceiling(-9.4))
@@ -59,6 +70,12 @@ def test_ceil_negative():
     assert_equal("\\lceil -\\sqrt{5}\\rceil", ceiling(-sqrt(5)))
     assert_equal("\\lceil -324E-3\\rceil", ceiling(Rational('-324E-3')))
     assert_equal("\\lceil -0.23\\rceil", ceiling(-0.23))
+
+    assert_equal("⌈-9.4⌉", ceiling(-9.4))
+    assert_equal("⌈-35.9825⌉", ceiling(-35.9825))
+    assert_equal("⌈-\\sqrt{5}⌉", ceiling(-sqrt(5)))
+    assert_equal("⌈-324E-3⌉", ceiling(Rational('-324E-3')))
+    assert_equal("⌈-0.23⌉", ceiling(-0.23))
 
 
 def test_ceil_fraction():
@@ -83,6 +100,13 @@ def test_ceil_fraction():
     assert_equal("\\lceil -325/3\\rceil", ceiling(Rational('-325/3')))
     assert_equal("\\lceil \\pi/2\\rceil", ceiling(pi / 2))
 
+    assert_equal("⌈1/2⌉", ceiling(Rational('1/2')))
+    assert_equal("⌈6/2⌉", ceiling(Rational('6/2')))
+    assert_equal("⌈9/5⌉", ceiling(Rational('9/5')))
+    assert_equal("⌈-42/6⌉", ceiling(Rational('-42/6')))
+    assert_equal("⌈-325/3⌉", ceiling(Rational('-325/3')))
+    assert_equal("⌈\\pi/2⌉", ceiling(pi / 2))
+
 
 def test_ceil_expr():
     assert_equal("\\ceil((1+6)/3)", ceiling(Rational(1 + 6, 3)))
@@ -103,6 +127,12 @@ def test_ceil_expr():
     assert_equal("38+\\lceil 15-2.3\\rceil", 38 + ceiling(15 - Rational('2.3')))
     assert_equal("\\sqrt{\\lceil 99.9999999999999\\rceil}", sqrt(ceiling(Rational('99.9999999999999'))))
 
+    assert_equal("⌈(1+6)/3⌉", ceiling(Rational(1 + 6, 3)))
+    assert_equal("⌈1+6/3⌉", ceiling(1 + Rational('6/3')))
+    assert_equal("⌈7*4/5⌉ * 2", ceiling(7 * 4 / 5) * 2)
+    assert_equal("38+⌈15-2.3⌉", 38 + ceiling(15 - Rational('2.3')))
+    assert_equal("\\sqrt{⌈99.9999999999999⌉}", sqrt(ceiling(Rational('99.9999999999999'))))
+
 
 def test_ceil_symbol():
     assert_equal("\\ceil(x)", ceiling(x), symbolically=True)
@@ -122,3 +152,9 @@ def test_ceil_symbol():
     assert_equal("\\lceil 9x/4\\rceil", ceiling(9 * x / 4), symbolically=True)
     assert_equal("\\lceil y\\pi\\rceil", ceiling(y * pi), symbolically=True)
     assert_equal("\\lceil 2y-y-y\\rceil", ceiling(2 * y - y - y), symbolically=True)
+
+    assert_equal("⌈x⌉", ceiling(x), symbolically=True)
+    assert_equal("⌈x + y⌉", ceiling(x + y), symbolically=True)
+    assert_equal("⌈9x/4⌉", ceiling(9 * x / 4), symbolically=True)
+    assert_equal("⌈y\\pi⌉", ceiling(y * pi), symbolically=True)
+    assert_equal("⌈2y-y-y⌉", ceiling(2 * y - y - y), symbolically=True)
--- tests/floor_test.py
+++ tests/floor_test.py
@@ -40,6 +40,17 @@ def test_floor_usual():
     assert_equal("\\lfloor \\pi\\rfloor", floor(pi))
     assert_equal("\\lfloor \\sqrt{100}\\rfloor", floor(sqrt(100)))
 
+    assert_equal("⌊1.1⌋", floor(1.1))
+    assert_equal("⌊6.9⌋", floor(6.9))
+    assert_equal("⌊3.5⌋", floor(3.5))
+    assert_equal("⌊8⌋", floor(8))
+    assert_equal("⌊0⌋", floor(0))
+    assert_equal("⌊290348E32⌋", floor(Rational('290348E32')))
+    assert_equal("⌊1237.293894239480234⌋", floor(Rational('1237.293894239480234')))
+    assert_equal("⌊8623.4592104E-2⌋", floor(Rational('8623.4592104E-2')))
+    assert_equal("⌊\\pi⌋", floor(pi))
+    assert_equal("⌊\\sqrt{100}⌋", floor(sqrt(100)))
+
 
 def test_floor_negative():
     assert_equal("\\floor(-9.4)", floor(-9.4))
@@ -59,6 +70,12 @@ def test_floor_negative():
     assert_equal("\\lfloor -\\sqrt{5}\\rfloor", floor(-sqrt(5)))
     assert_equal("\\lfloor -324E-3\\rfloor", floor(Rational('-324E-3')))
     assert_equal("\\lfloor -0.23\\rfloor", floor(-0.23))
+
+    assert_equal("⌊-9.4⌋", floor(-9.4))
+    assert_equal("⌊-35.9825⌋", floor(-35.9825))
+    assert_equal("⌊-\\sqrt{5}⌋", floor(-sqrt(5)))
+    assert_equal("⌊-324E-3⌋", floor(Rational('-324E-3')))
+    assert_equal("⌊-0.23⌋", floor(-0.23))
 
 
 def test_floor_fraction():
@@ -83,6 +100,13 @@ def test_floor_fraction():
     assert_equal("\\lfloor -325/3\\rfloor", floor(Rational('-325/3')))
     assert_equal("\\lfloor \\pi/2\\rfloor", floor(pi / 2))
 
+    assert_equal("⌊1/2⌋", floor(Rational('1/2')))
+    assert_equal("⌊6/2⌋", floor(Rational('6/2')))
+    assert_equal("⌊9/5⌋", floor(Rational('9/5')))
+    assert_equal("⌊-42/6⌋", floor(Rational('-42/6')))
+    assert_equal("⌊-325/3⌋", floor(Rational('-325/3')))
+    assert_equal("⌊\\pi/2⌋", floor(pi / 2))
+
 
 def test_floor_expr():
     assert_equal("\\floor((1+6)/3)", floor(Rational(1 + 6, 3)))
@@ -103,6 +127,12 @@ def test_floor_expr():
     assert_equal("38+\\lfloor 15-2.3\\rfloor", 38 + floor(15 - Rational('2.3')))
     assert_equal("\\sqrt{\\lfloor 99.9999999999999\\rfloor}", sqrt(floor(Rational('99.9999999999999'))))
 
+    assert_equal("⌊(1+6)/3⌋", floor(Rational(1 + 6, 3)))
+    assert_equal("⌊1+6/3⌋", floor(1 + Rational('6/3')))
+    assert_equal("⌊7*4/5⌋ * 2", floor(7 * 4 / 5) * 2)
+    assert_equal("38+⌊15-2.3⌋", 38 + floor(15 - Rational('2.3')))
+    assert_equal("\\sqrt{⌊99.9999999999999⌋}", sqrt(floor(Rational('99.9999999999999'))))
+
 
 def test_floor_symbol():
     assert_equal("\\floor(x)", floor(x), symbolically=True)
@@ -122,3 +152,9 @@ def test_floor_symbol():
     assert_equal("\\lfloor 9x/4\\rfloor", floor(9 * x / 4), symbolically=True)
     assert_equal("\\lfloor y\\pi\\rfloor", floor(y * pi), symbolically=True)
     assert_equal("\\lfloor 2y-y-y\\rfloor", floor(2 * y - y - y), symbolically=True)
+
+    assert_equal("⌊x⌋", floor(x), symbolically=True)
+    assert_equal("⌊x + y⌋", floor(x + y), symbolically=True)
+    assert_equal("⌊9x/4⌋", floor(9 * x / 4), symbolically=True)
+    assert_equal("⌊y\\pi⌋", floor(y * pi), symbolically=True)
+    assert_equal("⌊2y-y-y⌋", floor(2 * y - y - y), symbolically=True)

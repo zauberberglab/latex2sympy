5428d9910a58396c45fbbeb7b40245d4bb5d084e
Prakrit <pduangsu@purdue.edu>
2020-07-14 12:04:49 -0400

    add coverage

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -6,13 +6,6 @@ rel_path="$(dirname "$rdir")"
 # Change to that path and run the file
 cd $rel_path
 
-echo "creating venv..."
-if test -d .env
-then echo "venv exists"
-else python -m venv .env && echo "venv created"
-fi
-
-echo ''
 # Activate virtual environment
 echo "activating venv..."
 if test -f .env/bin/activate
@@ -22,21 +15,9 @@ then source .env/Scripts/activate && echo "venv activated (Scripts)"
 else exit 1
 fi
 
-echo ''
-echo "installing requirements..."
-if pip install -r dev-requirements.txt
-then echo "requirements installed"
+# Run unit test coverage
+echo "starting coverage..."
+if pytest --doctest-modules --cov-report=html --cov-config=.coveragerc --cov=__app__
+then echo "coverage finished"
 else exit 1
 fi
-
-echo ''
-echo "compiling parser..."
-sh scripts/compile.sh
-echo "parser compiled"
-
-echo ''
-echo "setup git hooks..."
-sh scripts/setup-hooks.sh
-echo "git hooks setup"
-
-exit 0

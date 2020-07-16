adcc1a9b2d94716e017bd5407b490776a7b63f31
Prakrit <pduangsu@purdue.edu>
2020-07-16 10:48:59 -0400

    update coverage-ci

--- scripts/coverage-ci.sh
+++ scripts/coverage-ci.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pytest --doctest-modules --junitxml=junit/test-results.xml --cov-report=xml --cov-config=.coveragerc --cov=__app__
+pytest --doctest-modules --junitxml=junit/test-results.xml --cov-report=xml --cov-config=.coveragerc --cov=latex2sympy tests

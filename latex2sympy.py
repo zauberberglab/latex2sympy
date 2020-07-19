cb57abf8d1f18a9667f8f5d9e8007a01141363d1
Prakrit <pduangsu@purdue.edu>
2020-07-19 19:55:32 -0400

    add args unpacking

--- latex2sympy.py
+++ latex2sympy.py
@@ -637,7 +637,7 @@ def convert_func(func):
             expr = handle_gcd_lcm(name, args)
         elif name in ["max", "min"]:
             name = name[0].upper() + name[1:]
-            expr = getattr(sympy.functions, name)(args, evaluate=False)
+            expr = getattr(sympy.functions, name)(*args, evaluate=False)
         elif name == "floor":
             expr = handle_floor(arg)
         elif name == "ceil":

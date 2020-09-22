bc3a52f359f2c962842093edee904280921d028b
Will Grauvogel <builtbywill@gmail.com>
2020-09-22 17:12:16 -0400

    add simple tests for new delimiter braces/brackets

--- tests/bracket_leftright_test.py
+++ tests/bracket_leftright_test.py
@@ -1,4 +1,4 @@
-from .context import assert_equal, _Pow
+from .context import assert_equal, _Pow, _Add
 import pytest
 from sympy import Integral, sin, Symbol, Mul, Integer, Pow
 from latex2sympy.latex2sympy import process_sympy
@@ -31,3 +31,11 @@ def test_bracket_leftright():
 
 def test_bracket_mixed():
     assert_equal("\\frac{1}{2}ab(a+b)", Mul(_Pow(2, -1), a, b, (a + b), evaluate=False))
+
+
+def test_brack():
+    assert_equal("\\left\\lbrack 1+2\\right\\rbrack ", _Add(1, 2))
+
+
+def test_brace():
+    assert_equal("\\left\\lbrace 1+2\\right\\rbrace", _Add(1, 2))

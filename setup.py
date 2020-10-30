e0a6b325715553eb21413b8b109317fe5bad803f
Will Grauvogel <builtbywill@gmail.com>
2020-10-30 14:59:13 -0400

    bump to 1.4.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    version="1.4.0",
+    version="1.4.1",
     name="latex2sympy",
     packages=find_packages(exclude=('tests')),
     py_modules=['asciimath_printer', 'latex2sympy'],
